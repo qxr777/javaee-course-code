59de3b6253c3ece90e7f8e097768e126546f3389
qxr777 <qixin_y20005@163.com>
2022-02-24 14:16:24 +0800

    add spring-boot-admin module

---
+++ Spring_Bucks/spring-boot-admin/src/test/java/edu/whut/cs/javaee/spring/springbucks/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.whut.cs.javaee.spring.springbucks.springbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

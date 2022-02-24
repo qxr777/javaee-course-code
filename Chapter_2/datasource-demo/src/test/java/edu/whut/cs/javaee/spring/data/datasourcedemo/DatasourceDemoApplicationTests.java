969ee2bf952ce96304a7b96ec4a172ae845348e2
qxr777 <qixin_y20005@163.com>
2022-02-24 16:02:10 +0800

    add CH2 modules

---
+++ Chapter_2/datasource-demo/src/test/java/edu/whut/cs/javaee/spring/data/datasourcedemo/DatasourceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.whut.cs.javaee.spring.data.datasourcedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasourceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

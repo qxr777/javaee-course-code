263088ecc4ccf9c74dc7d92a20c4fe8eca9854a7
qxr777 <qixin_y20005@163.com>
2022-02-24 14:15:04 +0800

    add customer-service-0 module

---
+++ Spring_Bucks/customer-service-0/src/test/java/edu/whut/cs/javaee/spring/springbucks/customer/CustomerService0ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.whut.cs.javaee.spring.springbucks.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerService0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

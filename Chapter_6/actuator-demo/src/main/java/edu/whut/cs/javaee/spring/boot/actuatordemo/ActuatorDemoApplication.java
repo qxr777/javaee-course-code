422cdf2ef7c6acfe76a897f4ae18ca4640dfe69c
qxr777 <qixin_y20005@163.com>
2022-04-18 23:03:46 +0800

    add actuator-demo module

---
+++ Chapter_6/actuator-demo/src/main/java/edu/whut/cs/javaee/spring/boot/actuatordemo/ActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.whut.cs.javaee.spring.boot.actuatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorDemoApplication.class, args);
+    }
+
+}

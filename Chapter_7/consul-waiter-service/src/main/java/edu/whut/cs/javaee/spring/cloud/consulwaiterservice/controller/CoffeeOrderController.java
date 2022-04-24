4aa7efdb0e54d4047d48578e9220cae9aa98b71e
qxr777 <qixin_y20005@163.com>
2022-04-25 07:41:39 +0800

    add consul modules

---
+++ Chapter_7/consul-waiter-service/src/main/java/edu/whut/cs/javaee/spring/cloud/consulwaiterservice/controller/CoffeeOrderController.java
@@ -0,0 +1,39 @@
+package edu.whut.cs.javaee.spring.cloud.consulwaiterservice.controller;
+
+import edu.whut.cs.javaee.spring.cloud.consulwaiterservice.controller.request.NewOrderRequest;
+import edu.whut.cs.javaee.spring.cloud.consulwaiterservice.model.Coffee;
+import edu.whut.cs.javaee.spring.cloud.consulwaiterservice.model.CoffeeOrder;
+import edu.whut.cs.javaee.spring.cloud.consulwaiterservice.service.CoffeeOrderService;
+import edu.whut.cs.javaee.spring.cloud.consulwaiterservice.service.CoffeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class CoffeeOrderController {
+    @Autowired
+    private CoffeeOrderService orderService;
+    @Autowired
+    private CoffeeService coffeeService;
+
+    @GetMapping("/{id}")
+    public CoffeeOrder getOrder(@PathVariable("id") Long id) {
+        CoffeeOrder order = orderService.get(id);
+        log.info("Get Order: {}", order);
+        return order;
+    }
+
+    @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public CoffeeOrder create(@RequestBody NewOrderRequest newOrder) {
+        log.info("Receive new Order {}", newOrder);
+        Coffee[] coffeeList = coffeeService.getCoffeeByName(newOrder.getItems())
+                .toArray(new Coffee[] {});
+        return orderService.createOrder(newOrder.getCustomer(), coffeeList);
+    }
+}
---
+++ Chapter_7/consul-waiter-service/src/main/java/edu/whut/cs/javaee/spring/cloud/consulwaiterservice/model/OrderState.java
@@ -0,0 +1,5 @@
+package edu.whut.cs.javaee.spring.cloud.consulwaiterservice.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}

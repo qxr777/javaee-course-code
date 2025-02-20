ada46cc2bc78c27506a493a1bc88438c8ec504b9
Xin Qi <qxr777@whut.edu.cn>
2025-02-20 21:55:35 +0800

    更新Spring_AI目录

---
+++ Spring_AI/agents/reflection/src/main/java/org/springframework/ai/openai/samples/helloworld/Application.java
@@ -0,0 +1,32 @@
+package org.springframework.ai.openai.samples.helloworld;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner cli(ReflectionAgent reflectionAgent) {
+        return args -> {
+            var scanner = new Scanner(System.in);
+            System.out.println("\nLet's chat!");
+
+            // Generate a Java implementation of the Merge Sort algorithm
+            while (true) {
+                System.out.print("\nUSER: ");
+                System.out.println("AGENT: " +
+                        reflectionAgent.run(scanner.nextLine(), 2));
+            }
+        };
+    }
+}
--- Spring_AI/spring-ai-rag-wiki/NOTE.md
+++ Spring_AI/spring-ai-rag-wiki/NOTE.md
@@ -3,3 +3,7 @@
 grafana设置数据源用docker主机名和内部端口号  http://prometheus:9090
 
 grafana登录admin，密码从admin改为123456
+http://localhost:3000
+
+在grafana dashboard页面，需要导入dashboard目录中的json文件
+对于每个panel，可能需要Edit，刷新可见到折线图

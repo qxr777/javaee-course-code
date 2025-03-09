6544e8f76ee0cfea329299d8ae7b023d443d1ff9
Xin Qi <qxr777@whut.edu.cn>
2025-03-09 22:03:11 +0800

    Exclude target directory from Git

--- Spring_Bucks/wechat-service-1/src/main/java/edu/whut/cs/javaee/spring/springbucks/wechat/llm/BookingNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package edu.whut.cs.javaee.spring.springbucks.wechat.llm;
-
-public class BookingNotFoundException extends RuntimeException {
-
-    public BookingNotFoundException(String bookingNumber) {
-        super("Booking " + bookingNumber + " not found");
-    }
-}
